ecfc63a5e8da37c038da4f3f677fb980166ad394
PeaceFrog71 <dafrog71@yahoo.com>
2024-12-13 01:21:12 -0700

    Fix constructor parameters and enhance Window class functionality

--- window.py
+++ window.py
@@ -1,12 +1,24 @@
 from tkinter import Tk, BOTH, Canvas
 
 class Window:
-    def __init__(width, height):
+    def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.root_widget = Tk()
-        self.root_widget.title = "root"
-        self.canvas_widget = Canvas(self.root_widget)
-        self.canvas_widget.pack()
-        is_running = False
-        
+        self.__root = Tk()
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__root.title = "root"
+        self.__canvas = Canvas(self.root_widget)
+        self.__canvas.pack(fill=BOTH, expand=True)
+        self._is_running = False
+
+    def redraw(self):
+        self.root.update_idletasks()
+        self.root.update()
+    
+    def wait_for_close(self):
+        self.is_running = True
+        while self.is_running:
+        self.redraw()
+
+    def close(self):
+        self.is_running = False
