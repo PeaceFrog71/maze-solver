05b89b5c1d7935814d5e95202d4634580f660786
PeaceFrog71 <dafrog71@yahoo.com>
2024-12-13 01:33:54 -0700

    Refactor Window class implementation and add main script for execution

--- src/window.py
+++ src/window.py
@@ -7,18 +7,18 @@ class Window:
         self.__root = Tk()
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
         self.__root.title = "root"
-        self.__canvas = Canvas(self.root_widget)
+        self.__canvas = Canvas(self.__root)
         self.__canvas.pack(fill=BOTH, expand=True)
         self._is_running = False
 
     def redraw(self):
-        self.root.update_idletasks()
-        self.root.update()
+        self.__root.update_idletasks()
+        self.__root.update()
     
     def wait_for_close(self):
-        self.is_running = True
-        while self.is_running:
+        self._is_running = True
+        while self._is_running:
             self.redraw()
 
     def close(self):
-        self.is_running = False
+        self._is_running = False
