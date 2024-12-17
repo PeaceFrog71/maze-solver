fe47cae347e1900ffced30f5ee48d3be769e9643
PeaceFrog71 <dafrog71@yahoo.com>
2024-12-16 23:36:00 -0700

    Add line drawing functionality in main script

--- src/main.py
+++ src/main.py
@@ -1,8 +1,21 @@
 from window import Window
-
+from drawables import Point, Line
 
 def main():
+    # Create Window
     win = Window(800, 600)
+    
+    # Create two points and a line
+    start_point = Point(50, 50)
+    end_point = Point(400, 300)
+    new_line = Line(start_point, end_point)
+
+    # Draw the line on the window
+    win.draw_line(new_line, "green")
+
+    # Keep the window open until close
     win.wait_for_close()
+
+
 
 main()
